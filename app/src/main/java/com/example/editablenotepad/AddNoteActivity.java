8dda0abb61f85268fe2c3a8c621d658908ddd633
thelowlyclutch <armandhrubo@gmail.com>
2020-05-15 03:26:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/editablenotepad/AddNoteActivity.java
@@ -0,0 +1,85 @@
+package com.example.editablenotepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.editablenotepad.database.DatabaseHandler;
+import com.example.editablenotepad.models.Note;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @BindView(R.id.etTitle)
+    EditText etTitle;
+
+    @BindView(R.id.etNote)
+    EditText etNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        ButterKnife.bind(this);
+    }
+
+
+    private void saveNote(){
+
+        String title = etTitle.getText().toString();
+        String note_text = etNote.getText().toString();
+
+        if(title.equals("") || note_text.equals("")){
+            showToast("Please fill all the fields before saving");
+        }else{
+            DatabaseHandler db = new DatabaseHandler(this);
+            Note note = new Note(title,note_text);
+            db.addNote(note);
+            db.close();
+
+            Intent i = new Intent(AddNoteActivity.this,MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        }
+
+    }
+
+
+    private void showToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_note,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_save){
+            saveNote();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void saveNote(View view) {
+        saveNote();
+    }
+}
---
+++ app/src/main/java/com/example/editablenotepad/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.editablenotepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.editablenotepad.adapters.NotesAdapter;
+import com.example.editablenotepad.database.DatabaseHandler;
+import com.example.editablenotepad.models.Note;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rvNotes)
+    RecyclerView rvNotes;
+
+    RecyclerView.Adapter adapter;
+    List<Note> notesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initViews();
+        loadNotes();
+
+    }
+
+    private void initViews() {
+
+        rvNotes.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void loadNotes(){
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        notesList = db.getAllNotes();
+        if(notesList.size() != 0){
+            adapter = new NotesAdapter(this,notesList);
+            rvNotes.setAdapter(adapter);
+        }
+
+
+    }
+
+    @OnClick(R.id.fabAddNote)
+    public void addNote(){
+        Intent i = new Intent(MainActivity.this,AddNoteActivity.class);
+        startActivity(i);
+    }
+
+}
